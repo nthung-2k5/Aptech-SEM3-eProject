454cf594fa3ee147e0583ad4b3e9e22ad801bb5b
nthung-2k5 <hungthitkhia@gmail.com>
2026-07-03 11:00:48 +0700

    add "About Us" page

---
+++ GiveAID/Pages/Components/HeroComponent.cshtml.cs
@@ -0,0 +1,9 @@
+﻿using Hydro;
+
+namespace GiveAID.Pages.Components;
+
+public class HeroComponent : HydroView
+{
+    public string Title { get; set; } = string.Empty;
+    public string Subtitle { get; set; } = string.Empty;
+}
