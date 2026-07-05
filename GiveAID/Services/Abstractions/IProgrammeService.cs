47f85041f8eeb688ce94fa8fe62896f3b96ab2b6
nthung-2k5 <hungthitkhia@gmail.com>
2026-07-05 22:28:07 +0700

    update programme service

--- GiveAID/Services/Abstractions/IProgrammeService.cs
+++ GiveAID/Services/Abstractions/IProgrammeService.cs
@@ -4,6 +4,6 @@ namespace GiveAID.Services.Abstractions;
 
 public interface IProgrammeService
 {
-    Task<ProgrammeSummaryDto[]> GetAllProgrammesAsync(ProgrammeQueryParameters query, CancellationToken ct = default);
+    Task<ProgrammeSummaryDto[]> GetAllProgrammesAsync(ProgrammeQueryParameters? query, CancellationToken ct = default);
     Task<ProgrammeDto?> GetProgrammeByIdAsync(Guid id, CancellationToken ct = default);
 }
--- GiveAID/Services/ProgrammeService.cs
+++ GiveAID/Services/ProgrammeService.cs
@@ -6,30 +6,33 @@ namespace GiveAID.Services;
 
 public class ProgrammeService : IProgrammeService
 {
-    public Task<ProgrammeSummaryDto[]> GetAllProgrammesAsync(ProgrammeQueryParameters query,
+    public Task<ProgrammeSummaryDto[]> GetAllProgrammesAsync(ProgrammeQueryParameters? query,
                                                           CancellationToken ct = default)
     {
         var q = MockData.Programmes.AsQueryable();
 
-        if (!string.IsNullOrWhiteSpace(query.SearchTerm))
+        if (query != null)
         {
-            q = q.Where(p => p.Name.Contains(query.SearchTerm, StringComparison.OrdinalIgnoreCase) ||
-                             p.Description.Contains(query.SearchTerm, StringComparison.OrdinalIgnoreCase));
+            if (!string.IsNullOrWhiteSpace(query.SearchTerm))
+            {
+                q = q.Where(p => p.Name.Contains(query.SearchTerm, StringComparison.OrdinalIgnoreCase) ||
+                                 p.Description.Contains(query.SearchTerm, StringComparison.OrdinalIgnoreCase));
+            }
+
+            if (!string.IsNullOrWhiteSpace(query.Ngo))
+            {
+                q = q.Where(p => p.Ngo.Equals(query.Ngo, StringComparison.OrdinalIgnoreCase));
+            }
+
+            if (!string.IsNullOrWhiteSpace(query.Cause))
+            {
+                q = q.Where(p => p.Cause.Equals(query.Cause, StringComparison.OrdinalIgnoreCase));
+            }
+
+            q = q.Skip((query.PageNumber - 1) * query.PageSize).Take(query.PageSize);
         }
 
-        if (!string.IsNullOrWhiteSpace(query.Ngo))
-        {
-            q = q.Where(p => p.Ngo.Equals(query.Ngo, StringComparison.OrdinalIgnoreCase));
-        }
-
-        if (!string.IsNullOrWhiteSpace(query.Cause))
-        {
-            q = q.Where(p => p.Cause.Equals(query.Cause, StringComparison.OrdinalIgnoreCase));
-        }
-
-        var paginated = q.Skip((query.PageNumber - 1) * query.PageSize).Take(query.PageSize).ToArray();
-
-        return Task.FromResult<ProgrammeSummaryDto[]>(paginated);
+        return Task.FromResult<ProgrammeSummaryDto[]>(q.ToArray());
     }
 
     public Task<ProgrammeDto?> GetProgrammeByIdAsync(Guid id, CancellationToken ct = default)
