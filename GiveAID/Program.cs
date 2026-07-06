88cfd6f62fe30054ee64b9edfb6500bf36404370
nthung-2k5 <hungthitkhia@gmail.com>
2026-07-06 19:39:26 +0700

    reorder DI services initialization lines

--- GiveAID/Program.cs
+++ GiveAID/Program.cs
@@ -14,29 +14,17 @@ builder.Services.AddRazorPages(options =>
 
 builder.Services.AddHydro();
 
-builder.Services.AddScoped<IPasswordService, PasswordService>();
-
-builder.Services.AddScoped<IAboutUsSubpageService, AboutUsSubpageService>();
-builder.Services.AddScoped<IProgrammeService, ProgrammeService>();
-builder.Services.AddScoped<IMemberService, MemberService>();
-builder.Services.AddScoped<INgoService, NgoService>();
-builder.Services.AddScoped<IUserQueryService, UserQueryService>();
-builder.Services.AddScoped<IGalleryImageService, GalleryImageService>();
-builder.Services.AddScoped<IDonationService, DonationService>();
 builder.Services.AddAuthentication(options =>
 {
-    options.DefaultAuthenticateScheme = Microsoft.AspNetCore.Authentication.JwtBearer.JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = Microsoft.AspNetCore.Authentication.JwtBearer.JwtBearerDefaults.AuthenticationScheme;
-})
-.AddCookie("Cookies", options =>
-{
-    options.LoginPath = "/Login";
-})
-.AddJwtBearer(options =>
+    options.DefaultAuthenticateScheme =
+            Microsoft.AspNetCore.Authentication.JwtBearer.JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme =
+            Microsoft.AspNetCore.Authentication.JwtBearer.JwtBearerDefaults.AuthenticationScheme;
+}).AddCookie("Cookies", options => { options.LoginPath = "/Login"; }).AddJwtBearer(options =>
 {
     var jwtSettings = builder.Configuration.GetSection("JwtSettings");
     string secretKey = jwtSettings["Secret"] ?? "super_secret_default_key_replace_me_in_production_over_32_chars";
-    
+
     options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
     {
         ValidateIssuer = true,
@@ -45,7 +33,8 @@ builder.Services.AddAuthentication(options =>
         ValidateIssuerSigningKey = true,
         ValidIssuer = jwtSettings["Issuer"] ?? "GiveAID",
         ValidAudience = jwtSettings["Audience"] ?? "GiveAID",
-        IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(secretKey))
+        IssuerSigningKey =
+                new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(secretKey))
     };
 
     options.Events = new Microsoft.AspNetCore.Authentication.JwtBearer.JwtBearerEvents
@@ -60,6 +49,22 @@ builder.Services.AddAuthentication(options =>
 
 builder.Services.AddHttpContextAccessor();
 
+var auditingInterceptor = new AuditingInterceptor();
+
+string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer(connectionString).AddInterceptors(auditingInterceptor));
+
+builder.Services.AddScoped<IPasswordService, PasswordService>();
+
+builder.Services.AddScoped<IAboutUsSubpageService, AboutUsSubpageService>();
+builder.Services.AddScoped<IProgrammeService, ProgrammeService>();
+builder.Services.AddScoped<IMemberService, MemberService>();
+builder.Services.AddScoped<INgoService, NgoService>();
+builder.Services.AddScoped<IUserQueryService, UserQueryService>();
+builder.Services.AddScoped<IGalleryImageService, GalleryImageService>();
+builder.Services.AddScoped<IDonationService, DonationService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -69,10 +74,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<AppDbContext>(options =>
-        options.UseSqlServer(connectionString).AddInterceptors(new AuditingInterceptor()));
 
 app.UseHttpsRedirection();
 
