3c13f00be6ecda0d516627369fd7a959f17616e2
nthung-2k5 <hungthitkhia@gmail.com>
2026-07-06 16:45:08 +0700

    small update

--- GiveAID/Services/Abstractions/IPaymentService.cs
+++ GiveAID/Services/Abstractions/IPaymentService.cs
@@ -1,8 +1,8 @@
-using GiveAID.Models;
-
-namespace GiveAID.Services.Abstractions;
-
-public interface IPaymentService
-{
-    public Task<bool> ProcessPaymentAsync(Payment payment, CancellationToken ct = default);
-}
+// using GiveAID.Models;
+//
+// namespace GiveAID.Services.Abstractions;
+//
+// public interface IPaymentService
+// {
+//     public Task<bool> ProcessPaymentAsync(Payment payment, CancellationToken ct = default);
+// }
