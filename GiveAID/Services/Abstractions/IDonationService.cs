0f38917c068c7e01a1f705c6bf3c86b4819bb7df
nthung-2k5 <hungthitkhia@gmail.com>
2026-07-07 16:03:24 +0700

    refine donation service

--- GiveAID/Services/Abstractions/IDonationService.cs
+++ GiveAID/Services/Abstractions/IDonationService.cs
@@ -7,5 +7,5 @@ public interface IDonationService
     Task<DonationDto[]> GetAllDonationsAsync(CancellationToken ct = default);
     Task<UserDonationDto[]> GetDonationsByUserAsync(Guid userId, CancellationToken ct = default);
     Task<DonationDto?> CreateDonationAsync(DonationSaveDto donation, CancellationToken ct = default);
-    Task<bool> VoidDonationAsync(Guid donationId, CancellationToken ct = default);
+    Task VoidDonationAsync(Guid donationId, CancellationToken ct = default);
 }
