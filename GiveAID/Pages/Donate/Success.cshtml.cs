6fb1c99eb10fd111356400b7139d577ea097a77a
nthung-2k5 <hungthitkhia@gmail.com>
2026-07-09 15:40:17 +0700

    add donation page

---
+++ GiveAID/Pages/Donate/Success.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using GiveAID.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GiveAID.Pages.Donate;
+
+[Authorize(Roles = nameof(UserRole.Member))]
+public class SuccessModel : PageModel
+{
+    public Guid TransactionId { get; set; }
+
+    public void OnGet(Guid transactionId) { TransactionId = transactionId; }
+}
